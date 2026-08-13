7245bcad04347ab13df8d2264b2ec64e062013bc
Zer0KC <nicolasalveschagas@gmail.com>
2026-08-12 21:43:59 -0300

    Aula 03

--- obj/Debug/net10.0/Aula 02.AssemblyInfo.cs
+++ obj/Debug/net10.0/Aula 02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula 02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c1c57339669c53c97b4ec79f0b30a67011d8d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd3de813dfd7f1cf25add2231ba17c9d339b8dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula 02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula 02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
